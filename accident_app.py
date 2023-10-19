754d1fdc799a782041680e0c1143d02df76304d8
adewopova <59388844+adewopova@users.noreply.github.com>
2023-10-19 02:49:25 -0400

    Update code to remove temp file

--- accident_app.py
+++ accident_app.py
@@ -91,6 +91,16 @@ def overlay_predictions_to_video(frames, predictions):
     
     # Clear existing frames and video if they exist
     if os.path.exists(temp_dir):
+        for filename in os.listdir(temp_dir):
+            file_path = os.path.join(temp_dir, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print(f'Failed to delete {file_path}. Reason: {e}')
+
         shutil.rmtree(temp_dir)
     os.makedirs(temp_dir)
     
